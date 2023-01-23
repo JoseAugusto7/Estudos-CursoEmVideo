5c837385cee1e2735d44abc18e431f9da8d95c38
JoseAugusto7 <tails.augusto@hotmail.com>
2023-01-23 16:47:12 -0300

    Condições compostas e aninhadas

--- Javascript/aula11/ex009.js
+++ Javascript/aula11/ex009.js
@@ -1,7 +1,7 @@
 var country = ('BR')
 console.log(`Vivendo no ${country}`)
 
-if (country == 'BR') {
+if (country == 'BR') { //Condição composta
     console.log('Você é Brasileiro')
 } else {
     console.log('Você é estrangeiro')

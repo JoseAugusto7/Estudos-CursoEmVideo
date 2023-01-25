40cd43102a3ed26ce0ed9d8ecfbf7973c1068568
JoseAugusto7 <tails.augusto@hotmail.com>
2023-01-25 14:54:42 -0300

    Variáveis compostas

---
+++ Javascript/aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 2, 8, 9] // Variável composta || Vetor || Array
+num [4] = 4 // Adicioanando mais um valor à variável composta
+num.push(3) // Adicionando um valor a variável composta sem especificar onde e sim colocando no final dela.
+num.length // Para mostrar quantos números tem no vetor ''console.log(`Nosso vetor é o ${num.length}`)''
+num.sort() // Para colocar os elementos em ordem crescente
+
+console.log(`Nosso vetor é o ${num}`)
